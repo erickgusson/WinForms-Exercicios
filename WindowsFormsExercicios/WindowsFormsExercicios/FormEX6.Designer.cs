ad805cfd675168f0008ef3f178519d9ab83a4fda
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-01-09 17:30:14 -0300

    .

--- WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.Designer.cs
+++ WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 107);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(102, 20);
+            this.dateTimePicker1.TabIndex = 0;
             // 
             // FormEX6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormEX6";
             this.Text = "FormEX6";
+            this.Load += new System.EventHandler(this.FormEX6_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.cs
+++ WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsExercicios
         {
             InitializeComponent();
         }
+
+        private void FormEX6_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
