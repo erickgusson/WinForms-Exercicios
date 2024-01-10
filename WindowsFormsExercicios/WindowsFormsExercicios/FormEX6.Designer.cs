8189c4408c9b1b0661d7e865caf9e7b24c4d385f
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-01-10 11:21:11 -0300

    Gerenciador de tarefas concluido
    Concluido com a ajuda do chat

--- WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.Designer.cs
+++ WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.Designer.cs
@@ -28,68 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpVencimento = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.txbTitulo = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.txbTarefa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbDesc = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.btnAdicionarTarefa = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.btnExcluirTarefa = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvTarefas = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTarefas)).BeginInit();
             this.SuspendLayout();
             // 
-            // dateTimePicker1
+            // dtpVencimento
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(10, 208);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(102, 20);
-            this.dateTimePicker1.TabIndex = 0;
+            this.dtpVencimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpVencimento.Location = new System.Drawing.Point(10, 195);
+            this.dtpVencimento.Name = "dtpVencimento";
+            this.dtpVencimento.Size = new System.Drawing.Size(125, 20);
+            this.dtpVencimento.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(15, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Titulo";
+            this.label1.Text = "Tarefa";
             // 
-            // txbTitulo
+            // txbTarefa
             // 
-            this.txbTitulo.Location = new System.Drawing.Point(10, 32);
-            this.txbTitulo.Name = "txbTitulo";
-            this.txbTitulo.Size = new System.Drawing.Size(180, 20);
-            this.txbTitulo.TabIndex = 2;
+            this.txbTarefa.Location = new System.Drawing.Point(10, 32);
+            this.txbTarefa.Name = "txbTarefa";
+            this.txbTarefa.Size = new System.Drawing.Size(125, 20);
+            this.txbTarefa.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(15, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Tarefa";
+            this.label2.Text = "Descrição";
             // 
-            // txbTarefa
+            // txbDesc
             // 
-            this.txbTarefa.Location = new System.Drawing.Point(10, 65);
-            this.txbTarefa.Name = "txbTarefa";
-            this.txbTarefa.Size = new System.Drawing.Size(180, 20);
-            this.txbTarefa.TabIndex = 4;
+            this.txbDesc.Location = new System.Drawing.Point(10, 65);
+            this.txbDesc.Multiline = true;
+            this.txbDesc.Name = "txbDesc";
+            this.txbDesc.Size = new System.Drawing.Size(125, 105);
+            this.txbDesc.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 191);
+            this.label3.Location = new System.Drawing.Point(10, 178);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 5;
@@ -97,93 +95,60 @@
             // 
             // btnAdicionarTarefa
             // 
-            this.btnAdicionarTarefa.Location = new System.Drawing.Point(10, 235);
+            this.btnAdicionarTarefa.Location = new System.Drawing.Point(10, 344);
             this.btnAdicionarTarefa.Name = "btnAdicionarTarefa";
-            this.btnAdicionarTarefa.Size = new System.Drawing.Size(102, 23);
+            this.btnAdicionarTarefa.Size = new System.Drawing.Size(125, 23);
             this.btnAdicionarTarefa.TabIndex = 6;
             this.btnAdicionarTarefa.Text = "Adicionar tarefa";
             this.btnAdicionarTarefa.UseVisualStyleBackColor = true;
-            this.btnAdicionarTarefa.Click += new System.EventHandler(this.btnAdicionarTarefa_Click);
+            this.btnAdicionarTarefa.Click += new System.EventHandler(this.buttonAdicionarTarefa_Click);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnLimpar);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.radioButton3);
-            this.panel1.Controls.Add(this.radioButton2);
-            this.panel1.Controls.Add(this.radioButton1);
             this.panel1.Controls.Add(this.btnExcluirTarefa);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dtpVencimento);
             this.panel1.Controls.Add(this.btnAdicionarTarefa);
-            this.panel1.Controls.Add(this.txbTitulo);
-            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.txbTarefa);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txbDesc);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 420);
+            this.panel1.Size = new System.Drawing.Size(145, 420);
             this.panel1.TabIndex = 7;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(10, 315);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(125, 23);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btnExcluirTarefa
             // 
-            this.btnExcluirTarefa.Location = new System.Drawing.Point(10, 264);
+            this.btnExcluirTarefa.Location = new System.Drawing.Point(10, 373);
             this.btnExcluirTarefa.Name = "btnExcluirTarefa";
-            this.btnExcluirTarefa.Size = new System.Drawing.Size(102, 23);
+            this.btnExcluirTarefa.Size = new System.Drawing.Size(125, 23);
             this.btnExcluirTarefa.TabIndex = 7;
             this.btnExcluirTarefa.Text = "Excluir tarefa";
             this.btnExcluirTarefa.UseVisualStyleBackColor = true;
             // 
-            // radioButton1
+            // dgvTarefas
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(10, 116);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(63, 17);
-            this.radioButton1.TabIndex = 8;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Urgente";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(10, 139);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(54, 17);
-            this.radioButton2.TabIndex = 9;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Media";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(10, 162);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(56, 17);
-            this.radioButton3.TabIndex = 10;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Pouca";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 92);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Importância";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(200, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(405, 420);
-            this.dataGridView1.TabIndex = 8;
+            this.dgvTarefas.BackgroundColor = System.Drawing.Color.White;
+            this.dgvTarefas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTarefas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTarefas.Location = new System.Drawing.Point(145, 0);
+            this.dgvTarefas.Name = "dgvTarefas";
+            this.dgvTarefas.Size = new System.Drawing.Size(460, 420);
+            this.dgvTarefas.TabIndex = 8;
+            this.dgvTarefas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTarefas_CellContentClick);
             // 
             // FormEX6
             // 
@@ -191,34 +156,30 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(605, 420);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvTarefas);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormEX6";
             this.Text = "FormEX6";
-            this.Load += new System.EventHandler(this.FormEX6_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTarefas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpVencimento;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txbTitulo;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txbTarefa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbDesc;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnAdicionarTarefa;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button btnExcluirTarefa;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvTarefas;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
--- WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.cs
+++ WindowsFormsExercicios/WindowsFormsExercicios/FormEX6.cs
@@ -13,23 +13,123 @@ namespace WindowsFormsExercicios
 {
     public partial class FormEX6 : Form
     {
+
+        private List<Tarefa> listaTarefas;
+
         public FormEX6()
         {
+
             InitializeComponent();
-        }
-
-        
-        private void FormEX6_Load(object sender, EventArgs e)
-        {
-
-
-            
+            listaTarefas = new List<Tarefa>();
+            ConfigurarDataGridView();
 
         }
 
-        private void btnAdicionarTarefa_Click(object sender, EventArgs e)
+        private void ConfigurarDataGridView()
         {
 
+            dgvTarefas.ColumnCount = 3;
+            dgvTarefas.Columns[0].Name = "Tarefa";
+            dgvTarefas.Columns[1].Name = "Descrição";
+            dgvTarefas.Columns[2].Name = "Vencimento";
+
+            DataGridViewButtonColumn coluna = new DataGridViewButtonColumn();
+            coluna.Name = "Ações";
+            coluna.Text = "Deletar";
+            coluna.UseColumnTextForButtonValue = true;
+            dgvTarefas.Columns.Add(coluna);
+
+            dgvTarefas.Columns["Tarefa"].Width = 140;
+            dgvTarefas.Columns["Descrição"].Width = 150;
+            dgvTarefas.Columns["Vencimento"].Width = 77;
+            dgvTarefas.Columns["Ações"].Width = 50;
+
+        }
+
+        private void AdicionarTarefa(string titulo, string descricao, DateTime dataVencimento)
+        {
+            Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento);
+            listaTarefas.Add(tarefa);
+
+            AtualizarDataGridView();
+            LimparCampos();
+        }
+
+        private void AtualizarDataGridView()
+        {
+            dgvTarefas.Rows.Clear();
+
+            foreach (Tarefa tarefa in listaTarefas)
+            {
+
+                dgvTarefas.Rows.Add(tarefa.Titulo, tarefa.Descricao, tarefa.DataVencimento.ToString("dd/MM/yyyy"));
+
+            }
+        }
+
+        private void dgvTarefas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex == dgvTarefas.Columns["Ações"].Index && e.RowIndex >= 0)
+            {
+                // Botão "Deletar" foi clicado
+                DialogResult resultado = MessageBox.Show("Tem certeza que deseja deletar esta tarefa?", "Confirmar Exclusão", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    listaTarefas.RemoveAt(e.RowIndex);
+                    AtualizarDataGridView();
+                }
+            }
+
+        }
+
+        private void LimparCampos()
+        {
+            txbTarefa.Clear();
+            txbDesc.Clear();
+            dtpVencimento.Value = DateTime.Now;
+        }
+
+        private void buttonAdicionarTarefa_Click(object sender, EventArgs e)
+        {
+            string titulo = txbTarefa.Text;
+            string descricao = txbDesc.Text;
+            DateTime dataVencimento = dtpVencimento.Value;
+
+            if (!string.IsNullOrWhiteSpace(titulo))
+            {
+                AdicionarTarefa(titulo, descricao, dataVencimento);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um título para a tarefa.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+            LimparCampos();
+
+        }
+
+
+    }
+
+    public class Tarefa
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public Tarefa(string titulo, string descricao, DateTime dataVencimento)
+        {
+            Titulo = titulo;
+            Descricao = descricao;
+            DataVencimento = dataVencimento;
         }
     }
 }
